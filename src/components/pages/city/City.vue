74bef5c8163b5bab73cb78b7f09b466b68e4b075
杜海鹏 <1343326532@qq.com>
2019-03-20 21:28:56 +0800

    搜索框样式完成

--- src/components/pages/city/City.vue
+++ src/components/pages/city/City.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="city">
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import Header from '@/components/pages/city/components/Header'
+import Search from '@/components/pages/city/components/Search'
 export default {
   name: 'City',
   data () {
@@ -13,7 +15,8 @@ export default {
     }
   },
   components: {
-    'city-header': Header
+    'city-header': Header,
+    'city-search': Search
   }
 }
 </script>
---
+++ src/components/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="请输入城市或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'City-Search',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search {
+    height: .72rem;
+    padding: 0 .1rem;
+    background-color: #00bcd4;
+}
+.search-input {
+    width: 100%;
+    height: .62rem;
+    line-height: .62rem;
+    padding: 0 .1rem;
+    text-align: center;
+    box-sizing: border-box;
+    border-radius: .2rem;
+}
+</style>
